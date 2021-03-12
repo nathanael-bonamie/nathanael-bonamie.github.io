690de32e809f816bcafa3bbd65aaced281b5f45f
nathanael-bonamie <80281444+nathanael-bonamie@users.noreply.github.com>
2021-03-12 21:47:12 +0100

    Add files via upload

---
+++ script-sdr.js
@@ -0,0 +1,53 @@
+sessionStorage.setItem("play","no");
+var countDownDate = new Date();
+if (countDownDate.getDay()==2 && countDownDate.getHours()==19){
+	countDownDate.setHours(19);
+	countDownDate.setMinutes(30);
+	countDownDate.setSeconds(00);
+	countDownDate=countDownDate.getTime();
+	}
+else if(countDownDate.getDay()==6 && countDownDate.getHours()==16){
+	countDownDate.setHours(16);
+	countDownDate.setMinutes(30);
+	countDownDate.setSeconds(00);
+	countDownDate=countDownDate.getTime();
+}
+else if(countDownDate.getDay()==5 && countDownDate.getHours()==21){
+	countDownDate.setHours(21);
+	countDownDate.setMinutes(36);
+	countDownDate.setSeconds(30);
+	countDownDate=countDownDate.getTime();
+}
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var audio = document.getElementById("cntq");
+    
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+    
+  if (distance <= 5000) {//fermeture à 5 sec
+    window.close();
+  }
+  else if (distance <= 60000 && seconds == 10) {//goes.ogg à 10 sec
+	audio.pause();
+	var bip = new Audio('c:/Users/Salle du Royaume/Downloads/goes.ogg');
+	bip.play();
+  }
+  else if (distance <= 60000 && seconds == 15) {//fadeout à 15 sec
+	fadeOutEffect();
+  }
+  else if (distance <= 115000 && sessionStorage.getItem("play")=='no'){//cantique à 1m50s
+	audio.volume=0.6;
+	audio.play();
+	sessionStorage.setItem("play","yes");
+  }
+}, 1000);
+
+function fadeOutEffect() {
+    setInterval(function () {
+		var audio = document.getElementById("cntq");
+		audio.volume-=0.01;
+    }, 100);
+	}
