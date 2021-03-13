d808daf673c809b3cca67019d4607603d807eb59
nathanael-bonamie <80281444+nathanael-bonamie@users.noreply.github.com>
2021-03-13 20:20:50 +0100

    Update script-sdr.js

--- script-sdr.js
+++ script-sdr.js
@@ -2,13 +2,13 @@ sessionStorage.setItem("play","no");
 var audio = document.getElementById("cntq");
 var countDownDate = new Date();
 
-if (countDownDate.getDay()==2 && countDownDate.getHours()==19){
+if (countDownDate.getDay()==2 && countDownDate.getHours()==19){//mardi soir 19h30
 	countDownDate.setHours(19);
 	countDownDate.setMinutes(30);
 	countDownDate.setSeconds(00);
 	countDownDate=countDownDate.getTime();
 	}
-else if(countDownDate.getDay()==6 && countDownDate.getHours()==16){
+else if(countDownDate.getDay()==6 && countDownDate.getHours()==16){//samedi aprèm 16h30
 	countDownDate.setHours(16);
 	countDownDate.setMinutes(30);
 	countDownDate.setSeconds(00);
@@ -30,32 +30,25 @@ var x = setInterval(function() {
   if (seconds < 10 ? document.getElementById("message").innerHTML = "La r&eacuteunion commence dans " + minutes + " mn 0" + seconds + " s" 
                    : document.getElementById("message").innerHTML = "La r&eacuteunion commence dans " + minutes + " mn " + seconds + " s");
   
-  //document.getElementById("message").innerHTML = "La réunion commence dans " + minutes + " mn " + seconds + " s ";
-    
-  /*if (distance <= 5000) {//fermeture à 5 sec
-    var customWindow = window.open('', '_blank', '');
-    customWindow.close(); 
-  }*/
   if (distance <= 60000 && seconds == 10) {//goes.ogg à 10 sec
-	//audio.pause();
 	var bip = new Audio('https://nathanael-bonamie.github.io/goes.ogg');
 	bip.play();
   }
-  else if (distance <= 60000 && seconds == 20) {//fadeout à 15 sec
+  else if (distance <= 60000 && seconds == 20) {//fadeout à 20 sec
 	var y = setInterval(function () {
 		var audio = document.getElementById("cntq");
 		audio.volume -= 0.01;
     		}, 150);
   }
-  else if (distance <= 115000 && sessionStorage.getItem("play")=='no'){//cantique à 1m50s
+  else if (distance <= 115000 && sessionStorage.getItem("play")=='no'){//cantique à 1m55
 	audio.volume = 0.5;
 	audio.play();
 	sessionStorage.setItem("play","yes");
   }
-  else if (minutes == 0 && seconds == 0){
+  else if (minutes == 0 && seconds == 0){//arret du décompte
   	clearInterval(x);
   }
-  else if (audio.volume < 0.03){
+  else if (audio.volume < 0.03){//arret fadeOut et pause audio
   	clearInterval(y);
 	audio.pause();
   	}
