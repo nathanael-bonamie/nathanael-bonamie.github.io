7220f63b93b13f8cf79279bdbcceb1425d32e8da
nathanael-bonamie <80281444+nathanael-bonamie@users.noreply.github.com>
2021-03-13 14:03:39 +0100

    Update script-sdr.js

--- script-sdr.js
+++ script-sdr.js
@@ -14,19 +14,23 @@ else if(countDownDate.getDay()==6 && countDownDate.getHours()==16){
 	countDownDate.setSeconds(00);
 	countDownDate=countDownDate.getTime();
 }
-else if(countDownDate.getDay()==6 && countDownDate.getHours()==13){
-	countDownDate.setHours(13);
-	countDownDate.setMinutes(30);
+else if(countDownDate.getDay()==6 && countDownDate.getHours()==14){
+	countDownDate.setHours(14);
+	countDownDate.setMinutes(05);
 	countDownDate.setSeconds(00);
 	countDownDate=countDownDate.getTime();
 }
 var x = setInterval(function() {
+  var audio = document.getElementById("cntq");
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  document.getElementById("message").innerHTML = "La réunion commence dans " + minutes + " mn " + seconds + " s ";
+  if (seconds < 10 ? document.getElementById("message").innerHTML = "La reunion commence dans " + minutes + " mn 0" + seconds + " s" 
+                   : document.getElementById("message").innerHTML = "La reunion commence dans " + minutes + " mn " + seconds + " s");
+  
+  //document.getElementById("message").innerHTML = "La réunion commence dans " + minutes + " mn " + seconds + " s ";
     
   if (distance <= 5000) {//fermeture à 5 sec
     window.close();
@@ -36,11 +40,11 @@ var x = setInterval(function() {
 	var bip = new Audio('https://nathanael-bonamie.github.io/goes.ogg');
 	bip.play();
   }
-  else if (distance <= 60000 && seconds == 15) {//fadeout à 15 sec
+  else if (distance <= 60000 && seconds == 20) {//fadeout à 15 sec
 	fadeOutEffect();
   }
   else if (distance <= 115000 && sessionStorage.getItem("play")=='no'){//cantique à 1m50s
-	audio.volume = 0.6;
+	audio.volume = 0.5;
 	audio.play();
 	sessionStorage.setItem("play","yes");
   }
@@ -50,5 +54,5 @@ function fadeOutEffect() {
     setInterval(function () {
 		var audio = document.getElementById("cntq");
 		audio.volume -= 0.01;
-    }, 100);
+    }, 150);
 	}
